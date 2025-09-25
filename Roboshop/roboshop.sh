a0d255136ff2bc635e14bba45ef430a98925ed6e
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-25 17:26:27 +0530

    updating hsted zones for roboshop

--- Roboshop/roboshop.sh
+++ Roboshop/roboshop.sh
@@ -3,6 +3,7 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0796ffab4fe100077"  #REPLACE WITH YOU SG ID
 ZONE_ID="Z05166562P910VXJWOR8Y"
+DOMAIN_NAME="karela.fun"  #REPLACE WITH YOUR DOMAIN NAME
 for instance in $@  #declaring all the instances
 do
 INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
@@ -17,7 +18,7 @@ if [ $instance != "frontend" ]; then
     fi
 	echo "$instance: $IP"
       aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+    --hosted-zone-id $Z05166562P910VXJWOR8Y \
     --change-batch '
     {
         "Comment": "Updating record set"
