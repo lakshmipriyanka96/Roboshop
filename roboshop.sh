8bf66561de0f7c1cd10c9d720ad135ac27f36aef
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-27 13:02:42 +0530

    updated folder

--- roboshop.sh
+++ roboshop.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-0796ffab4fe100077"  #REPLACE WITH YOU SG ID
+SG_ID="sg-0796ffab4fe100077" # replace with your SG ID
+ZONE_ID="Z05166562P910VXJWOR8Y" # replace with your ID
+DOMAIN_NAME="karela.fun"
 
-for instance in $@  #declaring all the instances
+for instance in $@ # mongodb redis mysql
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
-# Get Private IP
-if [ $instance != "frontend" ]; then
+    # Get Private IP
+    if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.karela.fun
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws86s.fun
     else
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-        RECORD_NAME="$DOMAIN_NAME" # karela.fun
+        RECORD_NAME="$DOMAIN_NAME" # daws86s.fun
     fi
-	echo "$instance: $IP"
+
+    echo "$instance: $IP"
 
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -33,6 +36,5 @@ if [ $instance != "frontend" ]; then
             }]
         }
         }]
-    }
-    '
+    }'
 done
